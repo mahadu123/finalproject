1d3b5b0518c12e048485776ed4e52a0238bf1434
Dhiru7058 <dhirajchavan7058@gmail.com>
2022-11-08 11:27:28 +0530

    Application Start

--- HomeLoanApplication/src/main/java/com/sdl/HomeLoanApplication.java
+++ HomeLoanApplication/src/main/java/com/sdl/HomeLoanApplication.java
@@ -9,8 +9,8 @@ public class HomeLoanApplication {
 	public static void main(String[] args) {
            
 		SpringApplication.run(HomeLoanApplication.class, args);
-                 System.out.println("Application Runing ...");
-                
+                 System.out.println("Hello CJC Dhiraj");
+                 
                  
 	}
 
