4af253f9ed493f050075f6e47e8be173067e168d
Dhiru7058 <dhirajchavan7058@gmail.com>
2022-11-11 12:06:47 +0530

    Customer Address added By DC

--- HomeLoanApplication/src/main/java/com/sdl/homeloan/HomeLoanApplication.java
+++ HomeLoanApplication/src/main/java/com/sdl/homeloan/HomeLoanApplication.java
@@ -7,25 +7,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HomeLoanApplication {
-	
+
 	private static final Logger LOGGER = LogManager.getLogger(HomeLoanApplication.class);
 
 	public static void main(String[] args) {
-		
-		SpringApplication.run(HomeLoanApplication.class, args);
-<<<<<<< HEAD:HomeLoanApplication/src/main/java/com/sdl/HomeLoanApplication.java
 
-                 
-                 
-                 
-			 System.out.println("Application Running By Master");
-			 System.out.println("Application is Running By Suprriya");
-			 System.out.println("Application again is Running by Priya");
-			 System.out.println("Application run by Riya");
-                
-=======
+		SpringApplication.run(HomeLoanApplication.class, args);
+
 		LOGGER.info("HomeLoan Application Running........");
->>>>>>> 8145001265375931128a13069a48e3c608db7db7:HomeLoanApplication/src/main/java/com/sdl/homeloan/HomeLoanApplication.java
 	}
 
 }
