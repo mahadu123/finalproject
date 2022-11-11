c00a61c9bc14b209a84744da02289fa2c145b0d1
git-amruta <amrutajoshi20@gmail.com>
2022-11-11 15:01:21 +0530

    Profession Details Added

---
+++ HomeLoanApplication/src/main/java/com/sdl/homeloan/services/ProfessionService.java
@@ -0,0 +1,14 @@
+package com.sdl.homeloan.services;
+
+import java.util.List;
+
+import com.sdl.homeloan.models.Profession;
+
+public interface ProfessionService {
+
+	String saveProfessionInfo(Profession profession);
+	List getAll();
+	String deleteById(int pid);
+	Profession updateById(Profession profession,int pid);
+
+}
