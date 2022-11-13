af5e334bdaeb46693b39b922b16283aaa90b4a7a
rasachin04 <rasachin984@gmail.com>
2022-11-13 14:29:46 +0530

    loan disbrusment

--- HomeLoanApplication/src/main/java/com/sdl/homeloan/serviceimpl/AllPersonalDocumentsServiceImpl.java
+++ HomeLoanApplication/src/main/java/com/sdl/homeloan/serviceimpl/AllPersonalDocumentsServiceImpl.java
@@ -77,16 +77,4 @@ public class AllPersonalDocumentsServiceImpl implements AllPersonalDocumentsServ
 
 	}
 
-	@Override
-	public AllPersonalDocuments getById(int documentID) {
-	
-		boolean anyMatch = docRepository.findAll().stream().anyMatch(p -> p.getDocumentID() == documentID);
-		
-		if(anyMatch) {
-			AllPersonalDocuments doc =docRepository.findById(documentID).get();
-			return doc;
-		}else
-		return null;
-	}
-
 }
